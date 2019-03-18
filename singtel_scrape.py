db5b66d607b67bff71218d58e68a9878652e688e
devesh <deveshasha.2@gmail.com>
2019-03-19 00:30:22 +0800

    selenium links handled

--- singtel_scrape.py
+++ singtel_scrape.py
@@ -1,36 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 questions = []
 answers = []
 
-googled_urls = ['https://www.singtel.com/personal/i/internet/broadband-at-home/faqs',
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--incognito")
+driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.implicitly_wait(50)
+
+soup_urls = ['https://www.singtel.com/personal/i/internet/broadband-at-home/faqs',
         'https://www.singtel.com/personal/i/faq/mobileShareFaqs',
         'https://www.singtel.com/personal/i/phones-plans/mobile/vas/datamore/faq',
         'https://www.singtel.com/personal/i/internet/home-broadband/fibre-broadband-0/faqs/faq-0']
 
-for url in googled_urls:
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    questions_soup = soup.find_all(['div','li'], class_ = 'qna-item')
-    for q in questions_soup:
-        questions.append(q.h6.text)
-        answers.append(q.div.text)
+for url in soup_urls:
+	page = requests.get(url)
+	soup = BeautifulSoup(page.text, 'html.parser')
+	questions_soup = soup.find_all(['div','li'], class_ = 'qna-item')
+	for q in questions_soup:
+		questions.append(q.h6.text)
+		answers.append(q.div.text)
 
-'''support_urls = ['https://www.singtel.com/personal/support/account-billing',
+selenium_urls = ['https://www.singtel.com/personal/support/account-billing',
         'https://www.singtel.com/personal/support/mobile-postpaid',
         'https://www.singtel.com/personal/support/prepaid-cards',
         'https://www.singtel.com/personal/support/broadband',
         'https://www.singtel.com/personal/support/singtel-tv',
         'https://www.singtel.com/personal/support/telephony',
         'https://www.singtel.com/personal/support/online-purchases',
-        'https://www.singtel.com/personal/support/lifestyle']'''
+        'https://www.singtel.com/personal/support/lifestyle']
 
+for url in selenium_urls:
+	driver.get(url)
+	soup = BeautifulSoup(driver.page_source, 'html.parser')
+	questions_soup = soup.find_all('a', class_ = 'faq-que-link' )
+	answers_soup = soup.find_all('div', class_ = 'faq-ans-desc')
+	for q, a in zip(questions_soup, answers_soup):
+		questions.append(q.text)
+		answers.append(a.text)
+driver.close()
 
 with open('qna.csv','w', encoding='utf-8') as file:
     for question,answer in zip(questions,answers):
         question = question.replace(',','').strip()
         answer = answer.replace(',','').strip()
-        file.write(question.replace('\n','') + ',' + answer.replace('\n',''))
+        file.write(question.replace('\n',' ') + ',' + answer.replace('\n',' '))
         file.write('\n')
 file.close()
